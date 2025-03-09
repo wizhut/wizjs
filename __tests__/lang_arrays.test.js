4806623239616d55936ac7926e5a6facac817f0e
Vassilios Karakoidas <vassilios.karakoidas@wizhut.tech>
2025-03-09 12:04:19 +0200

    introducing accumulator

--- __tests__/lang_arrays.test.js
+++ __tests__/lang_arrays.test.js
@@ -1,6 +1,6 @@
 const t = require('tap');
 
-const { compact } = require('../src/lang/arrays.js');
+const { compact, accumulate, Operator } = require('../src/lang/arrays.js');
 
 
 t.test('arrays/compact', (t) => {
@@ -10,5 +10,16 @@ t.test('arrays/compact', (t) => {
     t.match(compact([null, 1, undefined]), [1]);
     t.match(compact(null), []);
     t.match(compact(undefined), []);
+    t.end();
+});
+
+
+t.test('arrays/accumulate', (t) => {
+    const arr = [1, 2, 3];
+
+    t.equal(accumulate(arr, Operator.plus, 0), 6);
+    t.equal(accumulate(arr, Operator.minus, 0), -6);
+    t.equal(accumulate(arr, Operator.multiply, 0), 0);
+    t.equal(accumulate(arr, Operator.divide, 0), 0);
     t.end();
 });
--- src/lang/arrays.js
+++ src/lang/arrays.js
@@ -19,7 +19,31 @@ function compact(arr) {
     return result;
 }
 
+class Operator {
+    static plus = (a, b) => a + b;
+    static minus = (a, b) => a - b;
+    static multiply = (a, b) => a * b;
+    static divide = (a, b) => a / b;
+
+    static apply(op, a, b) {
+        return op(a, b);
+    }
+}
+
+
+function accumulate(arr, operator, initial=0) {
+    let accumulator = initial;
+
+    for (let i = 0; i < arr.length; i++) {
+        accumulator = Operator.apply(operator, accumulator, arr[i]);
+    }
+
+    return accumulator;
+}
+
 
 module.exports = {
-    compact
+    compact,
+    accumulate,
+    Operator
 }
